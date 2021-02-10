eab39b69ee5b7fede6da11900162d21f0e298afe
BobAleks <78293569+BobAleks@users.noreply.github.com>
2021-02-11 00:07:13 +0900

    Delete Lesson directory

--- Lesson/work3.py
+++
@@ -1,4 +0,0 @@
-number1 = int(input('Введите целое число: '))
-number2 = int(str(number1) + str(number1))
-number3 = int(str(number1) + str(number1) + str(number1))
-print(number1 + number2 + number3)
